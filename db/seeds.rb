4a11b8c28559cfdf909261f7b6b396cf00376644
JasonSi <529511542@qq.com>
2016-05-30 20:21:33 +0800

    修复导入单词长度错误

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@
 Vocabulary.delete_all
 # 读取单词表文件
 words = IO.readlines("./config/ext/wordlist.txt")
-# 形成SQL语句的规范形式
-words.map!{|w| ("('#{w.chomp}', '2016-04-27 20:13:14', '2016-05-30 20:16:00' , #{w.length} )") }
+# 形成SQL语句需要的规范形式
+words.map!{|w| ("('#{w.chomp}', '2016-04-27 20:00:00', '2016-04-28 20:00:00' , #{w.chomp.length} )") }
 # 构造SQL语句用于执行
 sql = "INSERT INTO vocabularies (word, created_at, updated_at, length) VALUES #{words.join(", ")}"
+# 执行SQL语句
 ActiveRecord::Base.connection.execute sql
